d85a3c9d7685d6a357f1f1539cd9bec03027d066
Lukas Grunzke <lukasg@uchicago.edu>
2022-09-20 10:18:43 +0100

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Lukas on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
